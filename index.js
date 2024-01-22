ccff407e1f542b9c210671bafe3c4f9bc60cce5b
Rob B <rob@rjrobbie.dev>
2024-01-22 21:21:58 +0000

    Fixed my array of list items from adding all previous entries adding a new item. The issue was caused by a for loop was nested inside the event listener.

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-let inputEl = document.getElementById("input-el")
-let inputBtn = document.getElementById("input-btn")
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+
+inputBtn.addEventListener("click", function () {
+    myLeads.push(inputEl.value);
+    inputEl.value = ""
+    renderLeads();
+});
+
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += "<li>" + myLeads[i] + "</li>";
+  }
+
+  ulEl.innerHTML = listItems;
+}
