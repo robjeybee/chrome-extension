c2cbdee9fa42e1f822b3933a949ae4e1616b8384
Rob B <rob@rjrobbie.dev>
2024-01-23 21:09:52 +0000

    wrapped an li in an anchor tag

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function () {
-    myLeads.push(inputEl.value);
-    inputEl.value = ""
-    renderLeads();
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  renderLeads();
 });
 
 function renderLeads() {
   let listItems = "";
   for (let i = 0; i < myLeads.length; i++) {
-    listItems += "<li>" + myLeads[i] + "</li>";
+    listItems += "<li><a href='" + myLeads[i] + "' target='_blank'>" + myLeads[i] + "</a></li>";
   }
 
   ulEl.innerHTML = listItems;
 }
+ 
