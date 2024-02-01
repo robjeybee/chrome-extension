89e1830a8a32ef693aba5ef0ce2177f7ef629bc9
Rob B <rob@rjrobbie.dev>
2024-02-01 20:22:48 +0000

    used chrome extension api to access and save current tab

--- index.js
+++ index.js
@@ -1,42 +1,40 @@
 let myLeads = [];
 
-const inputEl = document.getElementById("input-el")
+const inputEl = document.getElementById("input-el");
 
-const inputBtn = document.getElementById("input-btn")
-const deleteBtn = document.getElementById("delete-btn")
-const saveBtn = document.getElementById("save-btn")
+const inputBtn = document.getElementById("input-btn");
+const deleteBtn = document.getElementById("delete-btn");
+const saveBtn = document.getElementById("save-btn");
 
 const ulEl = document.getElementById("ul-el");
 
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if (leadsFromLocalStorage) {
-  myLeads = leadsFromLocalStorage
-  render(myLeads)
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
 }
 
-const tabs = [
-    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
-]
-
 saveBtn.addEventListener("click", function () {
-    console.log(tabs[0].url)
-})
-
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
 
 deleteBtn.addEventListener("dblclick", function () {
-  localStorage.clear()
-  myLeads = []
-  ulEl.innerHTML = ""
-  console.log("Button double clicked")
-})
+  localStorage.clear();
+  myLeads = [];
+  ulEl.innerHTML = "";
+});
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
-  localStorage.setItem("myLeads", JSON.stringify(myLeads))
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
   render(myLeads);
-  console.log( localStorage.getItem("myLeads") )
+  console.log(localStorage.getItem("myLeads"));
 });
 
 function render(leads) {
