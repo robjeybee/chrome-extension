38e87fed611a9e40ec696de0f1cf8cd0bc0da34a
Rob B <rob@rjrobbie.dev>
2024-02-02 21:43:46 +0000

    removed manual input function and corresponding save input function in favour of a save tab function

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 let myLeads = [];
 
-const inputEl = document.getElementById("input-el");
-
-const inputBtn = document.getElementById("input-btn");
 const deleteBtn = document.getElementById("delete-btn");
 const tabBtn = document.getElementById("tab-btn");
 
@@ -17,8 +14,7 @@ if (leadsFromLocalStorage) {
 
 tabBtn.addEventListener("click", function () {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-
-     myLeads.push(tabs[0].url);
+    myLeads.push(tabs[0].url);
     localStorage.setItem("myLeads", JSON.stringify(myLeads));
     render(myLeads);
   });
@@ -27,16 +23,9 @@ tabBtn.addEventListener("click", function () {
 deleteBtn.addEventListener("dblclick", function () {
   localStorage.clear();
   myLeads = [];
-  render(myLeads)
-});
-
-inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value);
-  inputEl.value = "";
-  localStorage.setItem("myLeads", JSON.stringify(myLeads));
   render(myLeads);
-
 });
+
 
 function render(leads) {
   let listItems = "";
